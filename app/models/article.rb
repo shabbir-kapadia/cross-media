084aedcadd8289b97e25dd0e77461d996c8c7f90
Shabbir Kapadia <kapadia4@illinois.edu>
2023-08-03 11:08:36 -0700

    Updating the model file to add validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
-end
+    #Adding this to provide validation when null string is passed on through the JSON
+    validates :title, presence: true, length: { maximum: 40 } #Adding limitation as title can be maximum 40 characters
+    validates :content, presence: true
+    validates :author, presence: true
+    validates :category, presence: true
+    validates :published_at, presence: true
+  end
